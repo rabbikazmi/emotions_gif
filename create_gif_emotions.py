5bab80a618056098c258ca9df4678bae7dc073c3
Rabbika Azmi <azmirabbika@gmail.com>
2025-03-17 22:58:15 +0530

    create_gif_emotions.py

--- create_gif_emotions.py
+++ create_gif_emotions.py
@@ -1,16 +1,16 @@
 import imageio as iio
 
-filenames = [r"C:\Users\Rabbika Azmi\OneDrive\Desktop\{}.png".format(i) for i in range(1, 10)]
-images = []
+filenames = [r"C:\Users\Rabbika Azmi\OneDrive\Desktop\{}.png".format(i) for i in range(1, 10)] #to generate file path dynamically
+images = []   #empty list to store the images
 
 for filename in filenames:
-    try:
+    try:                                       #error handling
         images.append(iio.imread(filename))
     except FileNotFoundError:
         print(f"Error: {filename} not found.")
 
 if images:
-    iio.mimsave("emotions.gif", images, duration=500, loop=0)
+    iio.mimsave("emotions.gif", images, duration=500, loop=0) #duration of images in milliseconds and loop=0 means it keeps on looping forever
     print("GIF created successfully!")
 else:
     print("No images found. Check file paths.")
